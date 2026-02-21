5a29f3e2164c7edec3db22508ad3f311ba523da7
Ammar Emad <za0663008@gmail.com>
2026-02-21 23:37:28 +0200

    feat: Initialize the core Flutter application structure, including image asset constants, and integrate DevicePreview.

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+
+  /// Assets for imagesApple
+  /// assets/images/apple.svg
+  static const String imagesApple = "assets/images/apple.svg";
+
+  /// Assets for imagesDeviceMobile
+  /// assets/images/device-mobile.svg
+  static const String imagesDeviceMobile = "assets/images/device-mobile.svg";
+
+  /// Assets for imagesFacebook
+  /// assets/images/Facebook.svg
+  static const String imagesFacebook = "assets/images/Facebook.svg";
+
+  /// Assets for imagesGoogle
+  /// assets/images/google.svg
+  static const String imagesGoogle = "assets/images/google.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.svg
+  static const String imagesLogo = "assets/images/logo.svg";
+
+  /// Assets for imagesSplash2
+  /// assets/images/splash2.png
+  static const String splash = "assets/images/splash2.png";
+}
