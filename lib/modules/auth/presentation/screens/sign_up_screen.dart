e9afad978485a3a04d8320bb258a7413b9593a5c
Ammar Emad <za0663008@gmail.com>
2026-02-21 23:35:54 +0200

    feat: Initialize core application structure including routing, theming, and authentication screens.

---
+++ lib/modules/auth/presentation/screens/sign_up_screen.dart
@@ -0,0 +1,12 @@
+import 'package:fit_app/modules/auth/presentation/screens/widgets/sign_up_screen_body.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SignUpScreenBody());
+  }
+}
+
