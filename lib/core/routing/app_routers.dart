9acab76fd6da95d8241eb04e58b80966c31ed8f6
Ammar Emad <za0663008@gmail.com>
2026-02-21 23:36:43 +0200

    feat: Establish initial project structure with GoRouter, app constants, and BLoC observer.

---
+++ lib/core/routing/app_routers.dart
@@ -0,0 +1,6 @@
+abstract class AppRouters {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String signUp = '/SignUpScreen';
+  static const String home = '/HomeScreen';
+}
