0781e84edd8521540130203051eef80f738066cc
Ammar_Emad <za0663008@gmail.com>
2026-03-12 15:43:19 +0200

    feat: Add `AppRouters` class to define application route constants.

--- lib/core/routing/app_routers.dart
+++ lib/core/routing/app_routers.dart
@@ -1,6 +1,6 @@
 abstract class AppRouters {
-  static const String splash = '/';
-  static const String login = '/login';
+  static const String splash = '/splash';
+  static const String login = '/';
   static const String signUp = '/SignUpScreen';
   static const String home = '/HomeScreen';
 }
