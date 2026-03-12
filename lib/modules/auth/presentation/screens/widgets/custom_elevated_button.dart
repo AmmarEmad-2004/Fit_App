15c7c351c623cd37280b3373319eb20ca0a33391
Ammar_Emad <za0663008@gmail.com>
2026-03-12 23:17:03 +0200

    feat: add authentication UI components including custom elevated button, login form, sign up form, and sign up section.

--- lib/modules/auth/presentation/screens/widgets/custom_elevated_button.dart
+++ lib/modules/auth/presentation/screens/widgets/custom_elevated_button.dart
@@ -2,12 +2,13 @@ import 'package:fit_app/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomElevatedButton extends StatelessWidget {
-  const CustomElevatedButton({super.key});
-
+  const CustomElevatedButton({super.key, required this.text, required this.onPressed});
+final String text;
+final VoidCallback onPressed;
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onPressed,
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primaryColor,
         minimumSize: Size(double.infinity, 48),
@@ -15,7 +16,7 @@ class CustomElevatedButton extends StatelessWidget {
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       ),
       child: Text(
-        'Log In',
+        text,
         style: TextStyle(
           fontWeight: FontWeight.w500,
           fontSize: 14,
