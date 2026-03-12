d7fefab0e7efb7f9bf9ac195f38a1b4db78742a2
Ammar_Emad <za0663008@gmail.com>
2026-03-12 23:40:09 +0200

    feat: add initial Firebase authentication repository implementation for `AuthRepo`.

---
+++ lib/modules/auth/data/repos/firebase_auth_repo_imple.dart
@@ -0,0 +1,15 @@
+import 'package:fit_app/modules/auth/data/repos/auth_repo.dart';
+
+class FirebaseAuthRepoImple implements AuthRepo {
+  @override
+  Future<void> signIn({required String email, required String password}) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUp({required String email, required String password}) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+}
