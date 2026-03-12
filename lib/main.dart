786812b355e27c854019ee88eb6e1acb71d426ad
Ammar_Emad <za0663008@gmail.com>
2026-03-12 15:41:19 +0200

    feat: Implement initial app setup, core routing, and splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(DevicePreview(enabled: true, builder: (context) => const FitApp()));
+  runApp(DevicePreview(enabled: false, builder: (context) => const FitApp()));
 }
