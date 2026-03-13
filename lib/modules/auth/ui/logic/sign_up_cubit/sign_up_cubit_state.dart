e8d85c982370f5a4ce0ee1f9cb99f66a02f209d0
Ammar_Emad <za0663008@gmail.com>
2026-03-13 03:59:31 +0200

    feat: add sign-up Cubit and its states for user registration logic.

--- lib/modules/auth/ui/logic/sign_up_cubit/sign_up_cubit_state.dart
+++ lib/modules/auth/ui/logic/sign_up_cubit/sign_up_cubit_state.dart
@@ -4,3 +4,12 @@ part of 'sign_up_cubit_cubit.dart';
 sealed class SignUpCubitState {}
 
 final class SignUpCubitInitial extends SignUpCubitState {}
+final class SignUpLoading extends SignUpCubitState {}
+final class SignUpSuccess extends SignUpCubitState {
+  final UserModel user;
+  SignUpSuccess({required this.user});
+}
+final class SignUpFailure extends SignUpCubitState {
+  final String errorMessage;
+  SignUpFailure({required this.errorMessage});
+}
