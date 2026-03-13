bc7b29367c0fcd71afcf5cf19242ba5f9dd966e9
Ammar_Emad <za0663008@gmail.com>
2026-03-13 03:58:16 +0200

    apply dartz in the return type of repo functions

--- lib/modules/auth/data/repos/auth_repo.dart
+++ lib/modules/auth/data/repos/auth_repo.dart
@@ -1,8 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:fit_app/core/errors/auth_failuer.dart';
 import 'package:fit_app/modules/auth/data/models/user_model.dart';
 
 abstract class AuthRepo {
-  Future<UserModel> signIn({required String email, required String password});
-  Future<UserModel> signUp({
+  Future<Either<AuthFailure, UserModel>> signIn({
+    required String email,
+    required String password,
+  });
+  Future<Either<AuthFailure, UserModel>> signUp({
     required String name,
     required String email,
     required String password,
