759b093aa77a34e6cbceb0c7f81214cab92c453c
Ammar_Emad <za0663008@gmail.com>
2026-03-14 00:20:39 +0200

    feat: Implement authentication UI for login and sign-up forms with a custom text field.

--- lib/modules/auth/ui/screens/widgets/custom_text_field.dart
+++ lib/modules/auth/ui/screens/widgets/custom_text_field.dart
@@ -1,23 +1,48 @@
-
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hintText, this.icon});
+  const CustomTextField( {
+    super.key,
+    required this.hintText,
+    this.icon,
+    this.obscureText = false,
+    this.onPressed,
+    required this.onChanged, this.validator,
+  });
   final String hintText;
   final IconData? icon;
-
+  final bool? obscureText;
+  final VoidCallback? onPressed;
+  final Function(String) onChanged;
+  final String? Function(String?)? validator;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      
+      obscureText: obscureText ?? false,
+      onChanged: onChanged,
+      validator: validator,
       decoration: InputDecoration(
         hintText: hintText,
-        suffixIcon: icon != null ? Icon(icon, color: Color(0xffacb5bb)) : null,
+        suffixIcon: icon != null
+            ? IconButton(
+                onPressed: onPressed,
+                icon: Icon(icon, color: Color(0xffacb5bb)),
+              )
+            : null,
         hintStyle: TextStyle(color: Color(0xff6c7278)),
         fillColor: Colors.white,
         filled: true,
         border: border(),
         focusedBorder: border(),
         enabledBorder: border(),
+        errorBorder: errorBorder(),
+        focusedErrorBorder: errorBorder(),
+        errorStyle: const TextStyle(
+          color: Color(0xffE53935),
+          fontSize: 12,
+          fontWeight: FontWeight.w500,
+        ),
       ),
     );
   }
@@ -26,6 +51,13 @@ class CustomTextField extends StatelessWidget {
     return OutlineInputBorder(
       borderRadius: BorderRadius.circular(10),
       borderSide: BorderSide.none,
+    );
+  }
+
+  OutlineInputBorder errorBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: const BorderSide(color: Color(0xffE53935), width: 1.5),
     );
   }
 }
