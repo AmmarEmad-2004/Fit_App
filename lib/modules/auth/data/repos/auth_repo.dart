50f179ae824d94eb44557a10494412af67fea9f4
Ammar_Emad <za0663008@gmail.com>
2026-03-13 02:34:47 +0200

    update the return of the functions with user model

--- lib/modules/auth/data/repos/auth_repo.dart
+++ lib/modules/auth/data/repos/auth_repo.dart
@@ -1,6 +1,8 @@
+import 'package:fit_app/modules/auth/data/models/user_model.dart';
+
 abstract class AuthRepo {
-  Future<void> signIn({required String email, required String password});
-  Future<void> signUp({
+  Future<UserModel> signIn({required String email, required String password});
+  Future<UserModel> signUp({
     required String name,
     required String email,
     required String password,
