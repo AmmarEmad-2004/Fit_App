a8c3304e59220706072d22aa3bd80979e044f918
Ammar_Emad <za0663008@gmail.com>
2026-03-14 00:20:14 +0200

    feat: Implement authentication UI screens and widgets for login and sign-up, including a custom text field.

--- lib/modules/auth/ui/screens/sign_up_screen.dart
+++ lib/modules/auth/ui/screens/sign_up_screen.dart
@@ -1,12 +1,17 @@
+import 'package:fit_app/core/utils/service_locator.dart';
+import 'package:fit_app/modules/auth/ui/logic/sign_up_cubit/sign_up_cubit.dart';
 import 'package:fit_app/modules/auth/ui/screens/widgets/sign_up_screen_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SignUpScreenBody());
+    return BlocProvider(
+      create: (context) => getIt<SignUpCubit>(),
+      child: Scaffold(body: SignUpScreenBody()),
+    );
   }
 }
-
