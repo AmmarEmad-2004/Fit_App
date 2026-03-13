1f4d521c8cf0eb36fbef2aa930450de1343c112e
Ammar_Emad <za0663008@gmail.com>
2026-03-13 02:40:15 +0200

    feat: Add `FirebaseAuthRepoImple` to provide Firebase authentication for user sign-in and sign-up.

--- lib/modules/auth/data/repos/firebase_auth_repo_imple.dart
+++ lib/modules/auth/data/repos/firebase_auth_repo_imple.dart
@@ -1,11 +1,18 @@
 import 'package:fit_app/modules/auth/data/models/user_model.dart';
 import 'package:fit_app/modules/auth/data/repos/auth_repo.dart';
+import 'package:fit_app/modules/auth/data/services/firebase_auth_service.dart';
 
 class FirebaseAuthRepoImple implements AuthRepo {
+  final FirebaseAuthService firebaseAuthService;
+
+  FirebaseAuthRepoImple({required this.firebaseAuthService});
   @override
-  Future<UserModel> signIn({required String email, required String password}) {
-    // TODO: implement signIn
-    throw UnimplementedError();
+  Future<UserModel> signIn({required String email, required String password}) async{
+    final user = await firebaseAuthService.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return user;
   }
 
   @override
@@ -13,8 +20,12 @@ class FirebaseAuthRepoImple implements AuthRepo {
     required String name,
     required String email,
     required String password,
-  }) {
-    // TODO: implement signUp
-    throw UnimplementedError();
+  }) async{
+    final user = await firebaseAuthService.signUpWithEmailAndPassword(
+      name: name,
+      email: email,
+      password: password,
+    );
+    return user;
   }
 }
