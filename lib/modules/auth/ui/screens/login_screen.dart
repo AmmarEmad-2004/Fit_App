a62a00cb98f9c6b52d932b87a29f2526a5d31846
Ammar_Emad <za0663008@gmail.com>
2026-03-14 00:20:02 +0200

    feat: Implement authentication UI, including login and sign-up screens with custom form fields and validation.

--- lib/modules/auth/ui/screens/login_screen.dart
+++ lib/modules/auth/ui/screens/login_screen.dart
@@ -1,11 +1,19 @@
+import 'package:fit_app/core/utils/service_locator.dart';
+import 'package:fit_app/modules/auth/ui/logic/sign_in_cubit/sign_in_cubit.dart';
 import 'package:fit_app/modules/auth/ui/screens/widgets/login_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: LoginScreenBody());
+    return Scaffold(
+      body: BlocProvider(
+        create: (context) => getIt<SignInCubit>(),
+        child: LoginScreenBody(),
+      ),
+    );
   }
 }
