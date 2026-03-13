1a6a6d87e6b2df8bc2fa319eed2146f670cf1159
Ammar_Emad <za0663008@gmail.com>
2026-03-14 00:02:16 +0200

    feat: Implement Firebase authentication module with sign-in, sign-up screens, services, and integration tests.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fit_app/core/utils/service_locator.dart';
+import 'package:fit_app/firebase_options.dart';
 import 'package:fit_app/fit_app.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setUPGetIt();
   runApp(DevicePreview(enabled: false, builder: (context) => const FitApp()));
 }
