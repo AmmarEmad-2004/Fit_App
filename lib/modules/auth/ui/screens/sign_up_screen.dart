dcc629d3fce166ec6e33656ed37f5dce447ff6c3
Ammar_Emad <za0663008@gmail.com>
2026-03-13 02:59:52 +0200

    feat: Implement initial login and sign-up UI screens and their associated widgets.

---
+++ lib/modules/auth/ui/screens/sign_up_screen.dart
@@ -0,0 +1,12 @@
+import 'package:fit_app/modules/auth/ui/screens/widgets/sign_up_screen_body.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SignUpScreenBody());
+  }
+}
+
