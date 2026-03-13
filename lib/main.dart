921f80526597bbe88c61fbf706e31a6042474dcb
Ammar_Emad <za0663008@gmail.com>
2026-03-13 11:38:24 +0200

    feat: Implement initial authentication flow with login and signup screens, forms, and Cubit state management.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fit_app/core/utils/service_locator.dart';
 import 'package:fit_app/fit_app.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  setUPGetIt();
   runApp(DevicePreview(enabled: false, builder: (context) => const FitApp()));
 }
