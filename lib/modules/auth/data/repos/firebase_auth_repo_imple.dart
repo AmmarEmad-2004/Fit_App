25423e27326d15b93f03c685deea4c91c84736b0
Ammar_Emad <za0663008@gmail.com>
2026-03-14 00:19:18 +0200

    feat: Implement user authentication module including sign-in and sign-up screens, forms, and Firebase integration.

--- lib/modules/auth/data/repos/firebase_auth_repo_imple.dart
+++ lib/modules/auth/data/repos/firebase_auth_repo_imple.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fit_app/core/errors/auth_failuer.dart';
 import 'package:fit_app/modules/auth/data/models/user_model.dart';
 import 'package:fit_app/modules/auth/data/repos/auth_repo.dart';
@@ -8,29 +9,43 @@ class FirebaseAuthRepoImple implements AuthRepo {
   final FirebaseAuthService firebaseAuthService;
 
   FirebaseAuthRepoImple({required this.firebaseAuthService});
+
   @override
   Future<Either<AuthFailure, UserModel>> signIn({
     required String email,
     required String password,
   }) async {
-    final user = await firebaseAuthService.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    return right(user);
+    try {
+      final user = await firebaseAuthService.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return right(user);
+    } on FirebaseAuthException catch (e) {
+      return left(AuthFailure(errorMessage: e.message ?? 'Sign in failed'));
+    } catch (e) {
+      return left(AuthFailure(errorMessage: e.toString()));
+    }
   }
 
   @override
-  Future<Either<AuthFailure, UserModel>> signUp({  
+  Future<Either<AuthFailure, UserModel>> signUp({
     required String name,
     required String email,
     required String password,
-  }) async{
-    final user = await firebaseAuthService.signUpWithEmailAndPassword(
-      name: name,
-      email: email,
-      password: password,
-    );
-    return right(user);
+  }) async {
+    try {
+      final user = await firebaseAuthService.signUpWithEmailAndPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+      return right(user);
+    } on FirebaseAuthException catch (e) {
+      return left(AuthFailure(errorMessage: e.message ?? 'Sign up failed'));
+    } catch (e) {
+      return left(AuthFailure(errorMessage: e.toString()));
+    }
   }
 }
+
