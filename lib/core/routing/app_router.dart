48b1a967f68263cf1bd9825c3103cf2b13cb9059
Ammar_Emad <za0663008@gmail.com>
2026-03-13 03:09:52 +0200

    add home screen to app router

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:fit_app/core/routing/app_routers.dart';
 import 'package:fit_app/modules/auth/ui/screens/login_screen.dart';
 import 'package:fit_app/modules/auth/ui/screens/sign_up_screen.dart';
+import 'package:fit_app/modules/home/ui/screens/home_screen.dart';
 import 'package:fit_app/modules/splash/screens/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -18,6 +19,10 @@ abstract class AppRouter {
       GoRoute(
         path: AppRouters.signUp,
         builder: (context, state) => const SignUpScreen(),
+      ),
+      GoRoute(
+        path: AppRouters.home,
+        builder: (context, state) => const HomeScreen(),
       ),
     ],
   );
