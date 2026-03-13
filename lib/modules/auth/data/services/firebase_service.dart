087d80422b00d20e0022d35ccce5b57ff4d316f2
Ammar_Emad <za0663008@gmail.com>
2026-03-13 02:34:02 +0200

    feat : create firebase auth service with two functions signIn and signUp

--- lib/modules/auth/data/services/firebase_service.dart
+++ lib/modules/auth/data/services/firebase_service.dart
@@ -1,5 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fit_app/modules/auth/data/models/user_model.dart';
+
 class FirebaseService {
-  Future<void> signInWithEmailAndPassword({required String email, required String password}) {
-    throw UnimplementedError();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<UserModel> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    UserCredential user = await _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return UserModel.fromMap(user.user!);
+  }
+
+  Future<UserModel> signUpWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return UserModel.fromMap(user.user!);
   }
 }
