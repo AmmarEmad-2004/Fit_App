00c59b6510f6bf243ff904c3a3cca3a31d1bb2cc
Ammar_Emad <za0663008@gmail.com>
2026-05-11 23:36:16 +0300

    feat: add SplashConfig class with animation duration and scale properties

---
+++ lib/core/helpers/splash_config.dart
@@ -0,0 +1,7 @@
+class SplashConfig {
+  static const Duration animationDuration =
+      Duration(milliseconds: 1200);
+
+  static const double startScale = 0.8;
+  static const double endScale = 1.0;
+}
