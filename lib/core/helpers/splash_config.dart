9a84416a53970fafb68a488a77d4ded5d53ef126
Ammar_Emad <za0663008@gmail.com>
2026-05-11 23:36:36 +0300

    refactor: remove SplashConfig class and its properties

--- lib/core/helpers/splash_config.dart
+++
@@ -1,7 +0,0 @@
-class SplashConfig {
-  static const Duration animationDuration =
-      Duration(milliseconds: 1200);
-
-  static const double startScale = 0.8;
-  static const double endScale = 1.0;
-}
